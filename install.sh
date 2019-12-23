c4685b44293d2465a29fa6e926a680ca670feea4
minhtu <tunnm@topica.edu.vn>
2019-12-24 01:49:34 +0700

    fix

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ set -eo pipefail
 
 # Init Configure
 export MINIFY_VERSION=2.6.1
-export S3CMD_VERSION=2.0.2
 apt-get update
 apt-get install -y wget procps lsb-release jpegoptim libjpeg-progs pngcrush optipng advancecomp gifsicle autoconf automake libtool make bc
 
@@ -17,13 +16,8 @@ chmod +x minify
 mv minify /usr/bin/
 
 # Install S3cmd - https://github.com/s3tools/s3cmd
-wget https://sourceforge.net/projects/s3tools/files/s3cmd/${S3CMD_VERSION}/s3cmd-${S3CMD_VERSION}.tar.gz
-tar xzf s3cmd-${S3CMD_VERSION}.tar.gz
-cd s3cmd-${S3CMD_VERSION}
-python3 setup.py install
+pip install s3cmd
 
 # Clean Install
-
-cd ..
 apt-get clean
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* LICENSE.md cmd/ minify_${MINIFY_VERSION}_linux_amd64.tar.gz s3cmd-${S3CMD_VERSION}/ s3cmd-${S3CMD_VERSION}.tar.gz
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* LICENSE.md cmd/ minify_${MINIFY_VERSION}_linux_amd64.tar.gz
