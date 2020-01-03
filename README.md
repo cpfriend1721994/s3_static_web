2648a1f624f5bd15809b4d5dcbf80adff735a48b
minhtu <tunnm@topica.edu.vn>
2020-01-04 04:22:28 +0700

    cmd validate

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # s3_static_web
 Docker image for push static web pages to s3, with compress images and minify HTML/JS/CSS
 
-This image combine 3 components:
+This image combine 4 components:
 
 1. [s5cmd](https://github.com/peak/s5cmd) for synchronize static web to S3
 2. [minify CLI](https://github.com/tdewolff/minify/tree/master/cmd/minify) for minify HTML5, CSS3, JS, JSON, SVG and XML
 3. [imagecomp](https://github.com/aprimadi/imagecomp) for compress images PNG, JPG
-
+4. [s3static](https://github.com/cpfriend1721994/s3_static_web) a mix between s5cmd, minify and imagecomp, for push static web pages to s3, with compress images and minify HTML/JS/CSS
 
 
 
@@ -14,4 +14,20 @@ This image combine 3 components:
 
 ```bash
   docker pull tunnm/s3_static_web
+```
+
+
+
+## Usage (s3static)
+
+```bash
+  AWS_ACCESS_KEY_ID=<your key> AWS_SECRET_ACCESS_KEY=<your secret> AWS_REGION=<your region>  s3static <directory path> <s3 destination path>
+```
+
+
+
+### Example:
+
+```bash
+  AWS_ACCESS_KEY_ID=xxxxxx AWS_SECRET_ACCESS_KEY=xxxxxx AWS_REGION=ap-southeast-1 s3static $(pwd) s3://xxxxxx/
 ```
--- main.go
+++ main.go
@@ -1,27 +1,41 @@
 package main
 
 import (
-  "fmt"
   "log"
   "os"
+  "os/exec"
   "github.com/urfave/cli/v2"
 )
 
 func main() {
   app := &cli.App{
     Name: "s3static",
-    Usage: "Push static web pages to s3, with compress images and minify HTML/JS/CSS",
-    Flags: []cli.Flag{
-      &cli.StringFlag{
-        Name:    "lang",
-        Aliases: []string{"l"},
-        Value:   "english",
-        Usage:   "language for the greeting",
-        EnvVars: []string{"LEGACY_COMPAT_LANG", "APP_LANG", "LANG"},
+    Usage: "AWS_ACCESS_KEY_ID=<your key> AWS_SECRET_ACCESS_KEY=<your secret> AWS_REGION=<your region>  s3static <directory path> <s3 destination path>",
+    UsageText: "s3static - push static web pages to s3, with compress images and minify HTML/JS/CSS",
+    Version: "1.0.0",
+    Authors: []*cli.Author{
+      &cli.Author{
+        Name:  "Nguyễn Nhật Minh Tú",
+        Email: "hedspi1721994@gmail.com",
       },
     },
+    HelpName: "tunnm",
     Action: func(c *cli.Context) error {
-      fmt.Println("Hello friend!")
+      if c.Args().Get(2) != "" || c.Args().Get(0) == "" || c.Args().Get(1) == "" {
+        log.Fatal("Wrong numbers of arguments, must be 2")
+        return nil
+      }
+      if os.Getenv("AWS_ACCESS_KEY_ID") == "" || os.Getenv("AWS_SECRET_ACCESS_KEY") == "" || os.Getenv("AWS_REGION") == "" {
+        log.Fatal("All AWS environment variables is mandatory")
+        return nil
+      }
+      cmd := exec.Command("ls", "-lah")
+      cmd.Stdout = os.Stdout
+      cmd.Stderr = os.Stderr
+      err := cmd.Run()
+      if err != nil {
+        log.Fatalf("%s\n", err)
+      }
       return nil
     },
   }
